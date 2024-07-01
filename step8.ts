861677b5a3f60a2073c4609d47a467a360d0ffb7
serhiidemianov <gummadisonxgfdop9@gmail.com>
2024-07-01 15:18:52 +0900

    first commit

---
+++ step8.ts
@@ -0,0 +1,25 @@
+import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { mnemonicToWalletKey } from "@ton/crypto";
+import { WalletContractV4, TonClient, fromNano } from "@ton/ton";
+
+async function main() {
+  // open wallet v4 (notice the correct wallet version here)
+  const mnemonic = "equip vehicle this tower phrase blast owner piano fitness despair expire trumpet memory twice cement panda uphold desk cupboard small boil parent liquid remember"; // your 24 secret words (replace ... with the rest of the words)
+  const key = await mnemonicToWalletKey(mnemonic.split(" "));
+  const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 });
+
+  // initialize ton rpc client on testnet
+  const endpoint = await getHttpEndpoint({ network: "testnet" });
+  const client = new TonClient({ endpoint });
+
+  // query balance from chain
+  const balance = await client.getBalance(wallet.address);
+  console.log("balance:", fromNano(balance));
+
+  // query seqno from chain
+  const walletContract = client.open(wallet);
+  const seqno = await walletContract.getSeqno();
+  console.log("seqno:", seqno);
+}
+
+main();
